5bf834987544d9fb7e070515223f68aada928120
Derek Modzelewski <derekmod@gmail.com>
2023-09-10 17:40:02 -0700

    .

--- any_serde/__init__.py
+++ any_serde/__init__.py
@@ -2,7 +2,6 @@ from .serde import (
     from_data,
     to_data,
 )
-from .dataclass_serde import to_data as dataclass_to_data
 from .common import (
     InvalidDeserializationException,
     InvalidSerializationException,
@@ -11,7 +10,6 @@ from .common import (
 __all__ = [
     "from_data",
     "to_data",
-    "dataclass_to_data",
     "InvalidDeserializationException",
     "InvalidSerializationException",
 ]
