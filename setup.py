66e9b39f3a7f5549133d3909b25062df2d484d23
Derekmod <derekmod@gmail.com>
2023-09-10 20:12:09 -0700

    Add Typed Marker (#10)

--- setup.py
+++ setup.py
@@ -2,10 +2,13 @@ from setuptools import find_packages, setup
 
 setup(
     name="any_serde",
-    version="0.0.4",
+    version="0.1.0",
     packages=find_packages(),
     include_package_data=True,
     install_requires=[],
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
+    package_data={
+        "": ["*py.typed"],
+    },
 )
