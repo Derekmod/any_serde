9a7c4acedbf5ca65a1a723b38daa091071dab025
Derek Modzelewski <derekmod@gmail.com>
2023-09-10 18:07:18 -0700

    .

--- setup.py
+++ setup.py
@@ -9,6 +9,6 @@ setup(
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
     package_data={
-        # "": ["*py.typed"],
+        "": ["*py.typed"],
     },
 )
