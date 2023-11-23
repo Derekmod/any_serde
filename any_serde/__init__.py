a49e32540042929b8e135be4275f4cbd6d91a241
Derek Modzelewski <derekmod@gmail.com>
2023-11-22 21:30:44 -0800

    .

--- any_serde/__init__.py
+++ any_serde/__init__.py
@@ -9,6 +9,7 @@ from .common import (
 )
 from .dataclass_serde import (
     dataclass_from_environ,
+    register_serialization_renames,
 )
 
 __all__ = [
@@ -18,4 +19,5 @@ __all__ = [
     "InvalidDeserializationException",
     "InvalidSerializationException",
     "dataclass_from_environ",
+    "register_serialization_renames",
 ]
