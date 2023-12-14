7b49dcdc049414a43ee5d7af6bbddc4e4f069b15
Derek Modzelewski <derekmod@gmail.com>
2023-12-13 22:16:04 -0800

    .

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name="any_serde",
-    version="0.2.8",
+    version="0.2.9",
     packages=find_packages(),
     include_package_data=True,
     install_requires=[],
@@ -11,4 +11,17 @@ setup(
     package_data={
         "": ["*py.typed"],
     },
+    data_files=[
+        (
+            "any_serde/typescript",
+            [
+                "any_serde/typescript/bool_typedef.ts.jinja2",
+                "any_serde/typescript/dataclass_typedef.ts.jinja2",
+                "any_serde/typescript/float_typedef.ts.jinja2",
+                "any_serde/typescript/list_typedef.ts.jinja2",
+                "any_serde/typescript/string_typedef.ts.jinja2",
+                "any_serde/typescript/union_typedef.ts.jinja2",
+            ],
+        ),
+    ],
 )
