0a55b0f3d887774905dec26fe311787736378410
Derek Modzelewski <derekmod@gmail.com>
2023-12-15 15:44:20 -0800

    .

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name="any_serde",
-    version="0.2.9",
+    version="0.2.10",
     packages=find_packages(),
     include_package_data=True,
     install_requires=[],
