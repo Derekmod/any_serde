15c9037b3a7cb40c79871044d20a1989e04d7cc3
Derek Modzelewski <derekmod@gmail.com>
2024-01-15 20:44:49 -0800

    .

--- any_serde/serde.py
+++ any_serde/serde.py
@@ -17,7 +17,8 @@ from any_serde.common import (
     Undefined,
     UndefinedValueException,
 )
-from any_serde import bytes_serde, enum, json_serde, primitives_serde, dataclass_serde, union_serde
+from any_serde import bytes_serde, json_serde, primitives_serde, dataclass_serde, union_serde
+import any_serde.enum
 
 
 T_Any = TypeVar("T_Any")
@@ -38,8 +39,8 @@ def from_data(
     if primitives_serde.is_primitive_type(type_):
         return primitives_serde.from_data(type_, data)  # type: ignore
 
-    if enum.is_enum_type(type_):
-        return enum.from_data(type_, data)  # type: ignore
+    if any_serde.enum.is_enum_type(type_):
+        return any_serde.enum.from_data(type_, data)  # type: ignore
 
     if dataclass_serde.is_dataclass_type(type_):
         return dataclass_serde.from_data(type_, data)
@@ -145,8 +146,8 @@ def to_data(type_: Type[T_Any], item: T_Any) -> JSON:
         # TODO check type matches item
         return primitives_serde.to_data(type_, item)  # type: ignore[type-var]
 
-    if enum.is_enum_type(type_):
-        return enum.to_data(type_, item)  # type: ignore[type-var, arg-type]
+    if any_serde.enum.is_enum_type(type_):
+        return any_serde.enum.to_data(type_, item)  # type: ignore[type-var, arg-type]
 
     if dataclass_serde.is_dataclass_type(type_):
         if type(item) is not type_:
